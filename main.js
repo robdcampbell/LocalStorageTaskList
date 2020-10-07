1e6ec624fe96f9ddf92296941d727914fc5a2f16
robdcampbell <rdeancampbell@gmail.com>
2020-10-06 23:29:46 -0400

    Begin UI, adding UI variables and Event listeners

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('its working...Pterodactyl')
+
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const fileter = document.querySelector('#filter');
+const taskInput =  document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventlisteners(){
+  form.addEventListener('submit',addTask);
+}
