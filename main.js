ff17e3f0cca37d21c3645722e1a6bd72f69afb97
robdcampbell <rdeancampbell@gmail.com>
2020-10-08 22:17:18 -0400

    Complete clear tasks and filter tasks in the UI

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ loadEventListeners();
 function loadEventListeners() {
   //Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event 
+  taskList.addEventListener('click', removeTask)
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -20,8 +26,6 @@ function addTask(e){
   if(taskInput.value == ''){
     alert('Add a task');
   }
-
-  console.log('Pterodactyl');
 
   // Create li element
   const li = document.createElement('li');
@@ -48,3 +52,39 @@ function addTask(e){
 
   e.preventDefault();
 } 
+
+// Remove Task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+   if(confirm('Are you sure?')){
+    e.target.parentElement.parentElement.remove();
+   }
+  }
+}
+
+// Clear Tasks
+function clearTasks(){
+  //taskList.innerHTML = '';
+
+  // Faster - While loop
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(
+    (task)=>{
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) !== -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
+
+
+}
