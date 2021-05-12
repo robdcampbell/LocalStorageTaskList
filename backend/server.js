ae7a179a4ee36a895a99f9b55be8b9d2f8c4665e
robdcampbell <rdeancampbell@gmail.com>
2021-05-11 22:59:46 -0400

    Create and Read routes created, update and delete next

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,51 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 const cors = require("cors");
+const pool = require("./db");
 
 // Middleware
 app.use(express.json()); // allows access to req (req.body, req.params, etc...)
 app.use(cors()); // allows cross domain resource sharing - proxing between ports 3000 and 5000 here
+
+// ROUTES //
+
+// Get all todos
+app.get("/todos", async (req, res) => {
+  const allTodos = await pool.query("SELECT * FROM todo");
+  res.status(200).json(allTodos.rows);
+});
+
+// get a specific todo
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id=($1)", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// create a todo
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.status(200).json(newTodo.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+// update a todo
+
+// delete a todo
 
 // Test Route
 app.get("/", (req, res) => {
