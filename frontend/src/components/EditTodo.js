16234ae4881eaa810887b98c896dd8b14d50aae2
robdcampbell <rdeancampbell@gmail.com>
2021-05-17 22:48:42 -0400

    Fix elements of UI in the Edit Modal

--- frontend/src/components/EditTodo.js
+++ frontend/src/components/EditTodo.js
@@ -20,15 +20,6 @@ const EditTodo = ({ showModal, setShowModal, todo, trip, setTrip }) => {
   };
 
   return (
-    // <>
-    //   <div>
-    //     <h1 onClick={(e) => console.log(todo.todo_id)}>
-    //       tests {todo.todo_id}{" "}
-    //     </h1>
-    //     <input type="text" placeholder={todo.description} />
-    //   </div>
-    // </>
-
     <div className="edit__modal">
       <div className="modal__content">
         <h1>Edit Todo {todo.todo_id}</h1>
