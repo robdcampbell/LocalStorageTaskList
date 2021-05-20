2c9431c19be3f69bff0361f6861f32af2a387999
robdcampbell <rdeancampbell@gmail.com>
2021-05-19 22:13:23 -0400

    Fix req.body of /auth/register query

--- registration/backend/routes/jwtAuth.js
+++ registration/backend/routes/jwtAuth.js
@@ -2,23 +2,19 @@ const router = require("express").Router();
 const pool = require("../config/db.js");
 
 // adding user to database
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
   try {
     // 1). destructure the req.body {email, name, password}
 
-    const { user_name, user_email, user_password } = await req.body;
+    const { name, email, password } = await req.body;
 
     // 2). Check if user exists (if yes, throw error)
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
-      user_email,
+      email,
     ]);
     console.log(user);
 
-    if (user.rowCount > 0) {
-      res.status(200).json({
-        message: "Bob Saget@!",
-      });
-    }
+    res.status(200).json(user.rows);
 
     // 3). Bcrypt the users password
     // 4). Enter the new user into Database
