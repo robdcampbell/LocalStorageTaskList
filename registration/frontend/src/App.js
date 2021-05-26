362500f318d163a6e2cc699847322b1c966a67ea
robdcampbell <rdeancampbell@gmail.com>
2021-05-26 16:52:42 -0400

    Begin implementing react-router-dom

--- registration/frontend/src/App.js
+++ registration/frontend/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React user registration test</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
