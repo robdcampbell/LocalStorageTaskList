c2b9ebd25a99e2aff92bedead8b9ea126f1922ec
robdcampbell <rdeancampbell@gmail.com>
2021-05-27 22:43:05 -0400

    React router dom with render props

--- registration/frontend/src/App.js
+++ registration/frontend/src/App.js
@@ -6,13 +6,25 @@ import {
   Redirect,
 } from "react-router-dom";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route
+            exact
+            path="/dashboard"
+            render={(props) => <Dashboard {...props} />}
+          />
+          <Route exact path="/login" render={(props) => <Login {...props} />} />
+          <Route
+            exact
+            path="/register"
+            render={(props) => <Register {...props} />}
+          />
         </Switch>
       </div>
     </Router>
