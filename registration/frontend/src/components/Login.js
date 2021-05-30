fc4837a51f0bb72b004fce1146f2509d2f63f958
robdcampbell <rdeancampbell@gmail.com>
2021-05-29 22:44:23 -0400

    Wiring up login Client-side

--- registration/frontend/src/components/Login.js
+++ registration/frontend/src/components/Login.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const { email, password, name } = inputs;
+
+  const changeInput = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    const body = { email, password, name };
+
+    try {
+      const response = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const parsedResponse = await response.json();
+      console.log(parsedResponse);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <>
       <h1>Login</h1>
       <button onClick={(e) => setAuth(true)}>Authenticate</button>
+      <form className="register__form" onSubmit={(e) => onSubmitForm(e)}>
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => changeInput(e)}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => changeInput(e)}
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={(e) => changeInput(e)}
+        />
+        <button>Submit</button>
+      </form>
     </>
   );
 };
